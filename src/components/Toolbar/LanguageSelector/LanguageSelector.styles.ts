66c8e028e7b408896af06bf254ec00fb625ec206
DavidRysanek <dawelich@gmail.com>
2025-03-24 16:07:47 +0100

    production deploy fix

--- src/components/Toolbar/LanguageSelector/LanguageSelector.styles.ts
+++ src/components/Toolbar/LanguageSelector/LanguageSelector.styles.ts
@@ -1,19 +1,19 @@
 import { styled } from "@mui/material/styles";
 
-export const Container = styled("div")(({ theme }) => ({
+export const Container = styled("div")(() => ({
   marginLeft: "auto",
   padding: "0 1rem",
   display: "flex",
   alignItems: "center",
 }));
 
-export const LanguageItemContent = styled("div")(({ theme }) => ({
+export const LanguageItemContent = styled("div")(() => ({
   display: "flex",
   alignItems: "center",
   width: "100%",
 }));
 
-export const FlagContainer = styled("span")(({ theme }) => ({
-  width: theme.spacing(3),
-  height: theme.spacing(3),
+export const FlagContainer = styled("span")(() => ({
+  width: "24px",
+  height: "24px",
 }));
