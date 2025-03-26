dd39168bdc6b8ddadd2bf4dd902b59e29d93c8b3
DavidRysanek <dawelich@gmail.com>
2025-03-27 00:19:11 +0100

    Auth sensitive info fix

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDfZWy9Fyiec-d9fQofvQR4X-V8fDSM8dI",
-  authDomain: "klub-apbt.firebaseapp.com",
-  projectId: "klub-apbt",
-  storageBucket: "klub-apbt.firebasestorage.app",
-  messagingSenderId: "477394758514",
-  appId: "1:477394758514:web:d00c30e87cb991f8368beb",
-  measurementId: "G-G23BDGEC0N",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
